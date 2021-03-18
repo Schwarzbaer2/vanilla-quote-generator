59d04470942b7010f5a423c4ddf280bf4acbc938
Andreas Schwarz <andreas.schwarz20@posteo.de>
2021-03-18 17:08:00 +0100

    Adding Loading Spinner

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function removeLoadingSpinner() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show New Quote
 function newQuote() {
+    showLoadingSpinner();
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check if Author field is blank and replace it with 'Unknown'
@@ -23,11 +35,14 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    removeLoadingSpinner();
 }
 
 // Get Quotes From API
 async function getQuotes() {
+    showLoadingSpinner();
 const apiURL = 'https://type.fit/api/quotes'
 try {
     const response = await fetch(apiURL);
